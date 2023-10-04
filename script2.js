40d330b95a661058affa8ee4e0ba1b61ff3ceb8d
RaidenLeo <leonardop_santos@hotmail.com>
2023-10-04 03:03:17 -0300

    Frontend - Interpass - Projeto Integrador

---
+++ script2.js
@@ -0,0 +1,56 @@
+const galleryContainer = document.querySelector('.gallery-container');
+const galleryControlsContainer = document.querySelector('.gallery-controls');
+const galleryControls = ['previous', 'next'];
+const galleryItems = document.querySelectorAll('.gallery-item');
+
+class Carousel {
+
+    constructor(container, items, controls) {
+        this.carouselContainer = container;
+        this.carouselControls = controls;
+        this.carouselArray = [...items];
+    }
+
+    updateGallery() {
+        this.carouselArray.forEach((el, i) => {
+            el.classList.remove(`gallery-item-${i + 1}`);
+        });
+
+        this.carouselArray.slice(0, 5).forEach((el, i) => {
+            el.classList.add(`gallery-item-${i + 1}`);
+        });
+    }
+
+    setCurrentState(direction) {
+        if (direction === 'gallery-controls-previous') {
+            this.carouselArray.unshift(this.carouselArray.pop());
+        } else if (direction === 'gallery-controls-next') {
+            this.carouselArray.push(this.carouselArray.shift());
+        }
+        this.updateGallery();
+    }
+
+    setControls() {
+        this.carouselControls.forEach(control => {
+            const button = document.createElement('button');
+            button.className = `gallery-controls-${control}`;
+            button.innerText = control;
+            galleryControlsContainer.appendChild(button);
+        });
+    }
+
+    useControls() {
+        const triggers = document.querySelectorAll('.gallery-controls button');
+        triggers.forEach(control => {
+            control.addEventListener('click', e => {
+                e.preventDefault();
+                this.setCurrentState(control.className);
+            });
+        });
+    }
+}
+
+const exampleCarousel = new Carousel(galleryContainer, galleryItems, galleryControls);
+
+exampleCarousel.setControls();
+exampleCarousel.useControls();
